da21b81f99cbaa1a28b737ae6529399849f70c31
Devin Berens <devinlberens3@gmail.com>
2023-08-07 13:19:54 -0500

    added different classes

--- src/StudentApp.js
+++ src/StudentApp.js
@@ -1,15 +1,18 @@
-import React, { useCallback, useState } from 'react';
-import moment from 'moment';
+import React, { useCallback, useState } from "react";
+import moment from "moment";
 
 const StudentApp = () => {
-  const [studentList, setStudentList] = useState(
-    localStorage.getItem('students')
-      ? JSON.parse(localStorage.getItem('students'))
+  const [morningList, setMorningList] = useState(
+    localStorage.getItem("morningClass")
+      ? JSON.parse(localStorage.getItem("morningClass"))
+      : []
+  );
+  const [afternoonList, setAfternoonList] = useState(
+    localStorage.getItem("afternoonClass")
+      ? JSON.parse(localStorage.getItem("afternoonClass"))
       : []
   );
   const [showCancelModal, setShowCancelModal] = useState(false);
-
-  // does this commit work?
 
   let handleSubmit = useCallback(
     (event) => {
@@ -18,130 +21,186 @@ const StudentApp = () => {
       let newStudent = {
         name: event.target[0].value,
         dob: event.target[1].value,
+        class: event.target[2].value,
       };
-      let studentsArr = studentList.slice();
-      studentsArr.push(newStudent);
-      studentsArr = studentsArr.sort(function (a, b) {
-        return new Date(a.dob) - new Date(b.dob);
-      });
-
-      localStorage.removeItem('students');
-      localStorage.setItem('students', JSON.stringify(studentsArr));
-      setStudentList(studentsArr);
+      let studentsArr;
+      if (event.target[2].value === "PA") {
+        studentsArr = morningList.slice();
+        studentsArr.push(newStudent);
+        studentsArr = studentsArr.sort(function (a, b) {
+          return new Date(a.dob) - new Date(b.dob);
+        });
+        localStorage.removeItem("morningList");
+        localStorage.setItem("morningList", JSON.stringify(studentsArr));
+        setMorningList(studentsArr);
+      } else {
+        studentsArr = afternoonList.slice();
+        studentsArr.push(newStudent);
+        studentsArr = studentsArr.sort(function (a, b) {
+          return new Date(a.dob) - new Date(b.dob);
+        });
+        localStorage.removeItem("afternoonList");
+        localStorage.setItem("afternoonList", JSON.stringify(studentsArr));
+        setAfternoonList(studentsArr);
+      }
     },
-    [studentList]
+    [morningList, afternoonList]
   );
 
   let reverseSort = useCallback(() => {
-    let studentsArr = studentList.slice();
-    studentsArr = studentsArr.reverse();
-    setStudentList(studentsArr);
-  }, [studentList]);
+    let morningArr = morningList.slice();
+    let afternoonArr = afternoonList.slice();
+    morningArr = morningArr.reverse();
+    afternoonArr = afternoonArr.reverse();
+    setMorningList(morningArr);
+    setAfternoonList(afternoonArr);
+  }, [morningList, afternoonList]);
 
   let showCancel = () => {
     setShowCancelModal(true);
   };
 
   let clearStudents = () => {
-    localStorage.setItem('students', JSON.stringify([]));
-    setStudentList([]);
+    localStorage.setItem("morningList", JSON.stringify([]));
+    localStorage.setItem("afternoonList", JSON.stringify([]));
+    setMorningList([]);
+    setAfternoonList([]);
     setShowCancelModal(false);
   };
 
   return (
     <div
       style={{
-        margin: '25px',
-        display: 'flex',
-        flexDirection: 'column',
-        height: '90vh',
-        alignItems: 'center',
+        margin: "25px",
+        display: "flex",
+        flexDirection: "column",
+        height: "90vh",
+        alignItems: "center",
       }}
     >
       <form
-        style={{ display: 'flex', alignItems: 'center' }}
+        style={{ display: "flex", alignItems: "center" }}
         onSubmit={handleSubmit}
       >
         <label>
-          <span style={{ fontSize: '24px', marginRight: '10px' }}>Name: </span>
+          <span style={{ fontSize: "24px", marginRight: "10px" }}>Name: </span>
           <input
-            style={{ fontSize: '20px', marginRight: '20px' }}
+            style={{ fontSize: "20px", marginRight: "20px" }}
             type="text"
           />
         </label>
         <label>
-          <span style={{ fontSize: '24px', marginRight: '10px' }}>
-            Date of Birth:{' '}
+          <span style={{ fontSize: "24px", marginRight: "10px" }}>
+            Date of Birth:{" "}
           </span>
           <input
-            style={{ fontSize: '20px', marginRight: '20px' }}
+            style={{ fontSize: "20px", marginRight: "20px" }}
             type="date"
           />
         </label>
-        <button style={{ fontSize: '20px', cursor: 'pointer' }} type="submit">
+        <label>
+          <span style={{ fontSize: "24px", marginRight: "10px" }}>
+            Select Class
+          </span>
+          <select style={{ fontSize: "20px", marginRight: "20px" }}>
+            <option hidden>Select</option>
+            <option>PA</option>
+            <option>PB</option>
+          </select>
+        </label>
+        <button style={{ fontSize: "20px", cursor: "pointer" }} type="submit">
           Add Student
         </button>
       </form>
       <div
         style={{
-          display: 'flex',
-          marginTop: '20px',
-          marginBottom: '20px',
+          display: "flex",
+          marginTop: "20px",
+          marginBottom: "20px",
         }}
       >
         <button
-          style={{ cursor: 'pointer', marginRight: '10px', fontSize: '20px' }}
+          style={{ cursor: "pointer", marginRight: "10px", fontSize: "20px" }}
           onClick={reverseSort}
         >
           Reverse Sort
         </button>
         <button
-          style={{ cursor: 'pointer', fontSize: '20px' }}
+          style={{ cursor: "pointer", fontSize: "20px" }}
           onClick={showCancel}
         >
           Clear All Students
         </button>
       </div>
-      <div style={{ width: '95vw', display: 'flex', flexDirection: 'column' }}>
-        {studentList.map((student, index) => (
-          <div
-            style={{
-              display: 'flex',
-              fontSize: '26px',
-              marginBottom: '10px',
-              width: '35%',
-            }}
-            key={index}
-          >
-            <div style={{ marginRight: '40px', flex: '1 1 auto' }}>
-              {student.name}
+      <div style={{ width: "95vw", display: "flex" }}>
+        <div
+          style={{
+            display: "flex",
+            flex: "1 1 auto",
+            flexDirection: "column",
+            marginRight: "10px",
+          }}
+        >
+          {morningList.map((student, index) => (
+            <div
+              style={{
+                display: "flex",
+                fontSize: "26px",
+                marginBottom: "10px",
+                width: "80%",
+              }}
+              key={index}
+            >
+              <div style={{ marginRight: "40px", flex: "1 1 auto" }}>
+                {student.name}
+              </div>
+              <div>{moment(student.dob).format("MM/DD/YYYY")}</div>
             </div>
-            <div>{moment(student.dob).format('MM/DD/YYYY')}</div>
-          </div>
-        ))}
+          ))}
+        </div>
+        <div
+          style={{ display: "flex", flex: "1 1 auto", flexDirection: "column" }}
+        >
+          {afternoonList.map((student, index) => (
+            <div
+              style={{
+                display: "flex",
+                fontSize: "26px",
+                marginBottom: "10px",
+                width: "80%",
+              }}
+              key={index}
+            >
+              <div style={{ marginRight: "40px", flex: "1 1 auto" }}>
+                {student.name}
+              </div>
+              <div>{moment(student.dob).format("MM/DD/YYYY")}</div>
+            </div>
+          ))}
+        </div>
       </div>
       {showCancelModal && (
         <div
           style={{
-            width: '550px',
-            height: '500px',
-            backgroundColor: 'white',
-            border: '3px solid gray',
-            position: 'absolute',
+            width: "550px",
+            height: "500px",
+            backgroundColor: "white",
+            border: "3px solid gray",
+            position: "absolute",
             zIndex: 500,
             top: `calc(50% - 250px)`,
             left: `calc(50% - 250px)`,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
           <div
             style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              flex: '1 1 auto',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              flex: "1 1 auto",
             }}
           >
             <h1>You are about to clear all students!</h1>
@@ -149,17 +208,17 @@ const StudentApp = () => {
             <h4>Really sure?</h4>
             <h6>Positive?</h6>
           </div>
-          <div style={{ display: 'flex', marginBottom: '25px' }}>
+          <div style={{ display: "flex", marginBottom: "25px" }}>
             <button
               style={{
-                backgroundColor: '#ee0000',
-                width: '200px',
-                flex: '1 1 auto',
-                height: '40px',
-                color: 'white',
-                fontSize: '20px',
-                marginRight: '10px',
-                cursor: 'pointer',
+                backgroundColor: "#ee0000",
+                width: "200px",
+                flex: "1 1 auto",
+                height: "40px",
+                color: "white",
+                fontSize: "20px",
+                marginRight: "10px",
+                cursor: "pointer",
               }}
               onClick={() => {
                 setShowCancelModal(false);
@@ -169,13 +228,13 @@ const StudentApp = () => {
             </button>
             <button
               style={{
-                backgroundColor: '#0000ee',
-                width: '200px',
-                height: '40px',
-                flex: '1 1 auto',
-                color: 'white',
-                fontSize: '20px',
-                cursor: 'pointer',
+                backgroundColor: "#0000ee",
+                width: "200px",
+                height: "40px",
+                flex: "1 1 auto",
+                color: "white",
+                fontSize: "20px",
+                cursor: "pointer",
               }}
               onClick={clearStudents}
             >
